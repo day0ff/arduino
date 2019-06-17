d6dec10bab59f7a776f7481bdd0ecb9f9ce356fc
day0ff <day85@tut.by>
2019-06-18 00:14:24 +0300

    Serial string converter to int.

---
+++ sketches/string.converter/string.converter.ino
@@ -0,0 +1,17 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String incomingString = Serial.readString();
+    int stringInt = 0;
+    
+    for (int i = 0; incomingString[i] != 0; i++) {
+      stringInt += int(incomingString[i]) * i * 10;
+    };
+    
+    Serial.print(incomingString);
+    Serial.println(stringInt);
+  }
+}
