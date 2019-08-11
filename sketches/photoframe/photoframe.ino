fee83ca8b8b2364f6b845691c5a3c24193a0ca68
day0ff <day85@tut.by>
2019-08-11 15:07:53 +0300

    Implement colorful pixels.

--- sketches/photoframe/photoframe.ino
+++ sketches/photoframe/photoframe.ino
@@ -9,7 +9,9 @@ SoftwareSerial HC05Module(10, 11); // TXD | RXD
 const unsigned int NUM_LINES = 9;
 const unsigned int NUM_ROWS = 12;
 
+unsigned int colorful[NUM_LEDS];
 CRGB leds[NUM_LEDS];
+CHSV lhsv( 0, 0, 0 );
 
 const unsigned int OFF = 2100;
 const unsigned int ON = 780;
@@ -22,6 +24,7 @@ unsigned int state = OFF;
 
 unsigned int code(String codeString) {
   Serial.println(codeString);
+
   unsigned int code = 0;
 
   for (int i = 0; codeString[i] != 0; i++) {
@@ -35,7 +38,7 @@ void setLine(unsigned int line, String incomingString) {
   int colorIndexStart = 0;
   int colorIndexEnd = 0;
   int hsvIndex = 0;
-  int hsv[3];
+  int hsv[4];
   int ledIndex = 0;
 
   for (int i = 0; incomingString[i] != 0; i++) {
@@ -49,6 +52,7 @@ void setLine(unsigned int line, String incomingString) {
       colorIndexStart = i + 1;
       if (ledIndex <= NUM_LEDS) {
         leds[ledIndex + line * NUM_ROWS] = CHSV(hsv[0], hsv[1], hsv[2]);
+        colorful[ledIndex + line * NUM_ROWS] = hsv[3];
       }
       ledIndex++;
     } else {
@@ -73,8 +77,6 @@ void readIncomingString(String incomingString) {
     case SHOW:
       FastLED.show();
       break;
-    default:
-      state = OFF;
   }
 }
 
@@ -82,6 +84,11 @@ void setup() {
   FastLED.addLeds<WS2812, PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(50);
   pinMode(PIN, OUTPUT);
+
+  for (int i = 0; i < NUM_LEDS; i++ ) {
+    leds[i] = CHSV(0, 0, 0);
+    colorful[i] = 0;
+  }
 
   Serial.begin(9600);
   HC05Module.begin(9600);  //Default Baud for comm, it may be different for your Module.
@@ -93,6 +100,15 @@ void loop() {
     readIncomingString(HC05Module.readString());
   }
 
-  if (Serial.available())
-    HC05Module.write(Serial.read());
+  if (state == SHOW) {
+    for (int i = 0; i < NUM_LEDS; i++ ) {
+      if (colorful[i] == 1) {
+        lhsv = rgb2hsv_approximate( leds[i]);
+        leds[i] = CHSV(lhsv.h + 2, lhsv.s, lhsv.v);
+      }
+    }
+    FastLED.show();
+    delay(10);
+  }
+
 }
